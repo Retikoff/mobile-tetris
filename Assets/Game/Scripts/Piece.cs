2596ab62da4141b1b4006bae0cee7b2f9f997f5d
Andrei Retsikau <154879381+Retikoff@users.noreply.github.com>
2026-04-24 01:08:26 +0300

    Rotation of a piece (with wall kicks)

--- Assets/Game/Scripts/Piece.cs
+++ Assets/Game/Scripts/Piece.cs
@@ -1,4 +1,5 @@
 using System.Data;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Video;
@@ -9,14 +10,19 @@ public class Piece : MonoBehaviour
     public TetrominoData data {get; private set;}
     public Vector3Int[] cells {get; private set;}
     public Vector3Int position {get; private set;}
+    public int rotationIndex {get; private set;}
     private InputAction movementAction;
     private Vector2 currentInput;
     private InputAction dropAction;
+    private InputAction rotateLeft;
+    private InputAction rotateRight;
 
     public void Awake()
     {
         movementAction = InputSystem.actions.FindAction("Move");
         dropAction = InputSystem.actions.FindAction("Jump");
+        rotateLeft = InputSystem.actions.FindAction("Previous");
+        rotateRight = InputSystem.actions.FindAction("Next");
     }
 
     public void Initialize(Board board, Vector3Int position, TetrominoData data)
@@ -24,6 +30,7 @@ public class Piece : MonoBehaviour
         this.board = board;
         this.position = position;
         this.data = data;
+        this.rotationIndex = 0;
 
         if(this.cells == null)
         {
@@ -41,18 +48,27 @@ public class Piece : MonoBehaviour
         this.board.Clear(this);
         currentInput = movementAction.ReadValue<Vector2>();
 
-        if (currentInput.x < 0)
+        if (rotateLeft.WasPerformedThisFrame())
+        {
+            Rotate(-1);
+        }
+        else if (rotateRight.WasPerformedThisFrame())
+        {
+            Rotate(1);   
+        }
+
+        if (currentInput.x < 0 && movementAction.WasPerformedThisFrame())
         {
             Move(Vector2Int.left);
             Debug.Log("LEFTTT");
         }
-        else if (currentInput.x > 0)
+        else if (currentInput.x > 0 && movementAction.WasPerformedThisFrame())
         {
             Move(Vector2Int.right);
             Debug.Log("RIGHT");
         }
 
-        if (currentInput.y < 0)
+        if (currentInput.y < 0 && movementAction.WasPerformedThisFrame())
         {
             Move(Vector2Int.down);
         }
@@ -87,5 +103,87 @@ public class Piece : MonoBehaviour
         }
 
         return valid;
+    }
+
+    private void Rotate(int direction)
+    {
+        int originalRotation = this.rotationIndex;
+        this.rotationIndex = Wrap(this.rotationIndex + direction, 0, 4);
+
+        ApplyRotationMatrix(direction);
+
+        if (!TestWallKicks(this.rotationIndex, direction))
+        {
+            this.rotationIndex = originalRotation;
+            ApplyRotationMatrix(-direction);
+        }
+    }
+
+    private void ApplyRotationMatrix(int direction)
+    {
+        for (int i = 0; i < this.cells.Length; i++)
+        {
+            Vector3 cell = this.cells[i];   
+
+            int x, y;
+
+            switch (this.data.tetromino)
+            {
+                case Tetromino.I:
+                case Tetromino.O:
+                    cell.x -= 0.5f;
+                    cell.y -= 0.5f;
+                    x = Mathf.CeilToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.CeilToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;
+                default:
+                    x = Mathf.RoundToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.RoundToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;
+            }
+
+            this.cells[i] = new Vector3Int(x, y, 0);
+        }
+    }
+
+    private bool TestWallKicks(int rotationIndex, int rotationDirection)
+    {
+        int wallKickIndex = GetWallKickIndex(rotationIndex, rotationDirection);       
+
+        for (int i = 0; i < this.data.wallKicks.GetLength(1); i++)
+        {
+            Vector2Int translation = this.data.wallKicks[wallKickIndex, i];
+
+            if (Move(translation))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private int GetWallKickIndex(int rotationIndex, int rotationDirection)
+    {
+        int wallKickIndex = rotationIndex * 2;
+
+        if (rotationDirection < 0)
+        {
+            wallKickIndex--;
+        }
+
+        return Wrap(wallKickIndex, 0, this.data.wallKicks.GetLength(0));
+    }
+
+    private int Wrap(int input, int min, int max)
+    {
+        if (input < min)
+        {
+            return max - (min - input) % (max - min);
+        }
+        else
+        {
+            return min + (input - min) % (max - min);
+        }
     }
 }
